90c75026134f0b3d37c77424859936d02ad24799
derrick b <dbozich1@gmail.com>
2018-09-23 02:18:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ function BoardMember(name, homeState, training){
   this.approve = () => "You can do that!";
   this.doCharity = () => "I like to help people.";
   this.releasePressStatement = () => "You will see great things from Scuber.";
+  this.sayHi() = () => `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}."`;
+
 
 
 }
