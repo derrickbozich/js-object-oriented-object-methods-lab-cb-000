eacb375ffe653c6dfa5907cbbc64feeab2c8d30f
derrick b <dbozich1@gmail.com>
2018-09-23 02:13:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-function BoardMember(name, homestate, training){
+function BoardMember(name, homeState, training){
   this.name = name;
-  this.homestate = homestate;
+  this.homeState = homeState;
   this. training = training;
-  this.veto = () => {"No, I must disagree"}
+  this.veto = () => {"No, I must disagree"};
 }
